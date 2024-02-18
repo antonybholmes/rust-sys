c732a79170bed738cb0c751daa07dc1f56821c87
Antony Holmes <antony@antonybholmes.com>
2024-02-18 12:47:04 -0500

    fix: Bug fixes and updates.

--- src/env.rs
+++ src/env.rs
@@ -3,19 +3,19 @@ use dotenvy::dotenv;
 
 const EMPTY_STRING: &'static str = "";
 
-// Load envs from .env file
+/// Load envs from .env file
 pub fn load() {
     dotenv().ok();
 }
 
-// List all envs
+/// List all envs
 pub fn ls() {
     for (key, value) in env::vars() {
         println!("{}: {}", key, value);
     }
 }
 
-// Get env as string, returning "" if env not found
+/// Get env as string, returning "" if env not found
 pub fn str(key: &str) -> String {
     match env::var(key) {
         Ok(val) => val,
@@ -23,7 +23,7 @@ pub fn str(key: &str) -> String {
     }
 }
 
-// Get env as u32 returning 0 if not found
+/// Get env as u32 returning 0 if not found
 pub fn u32(key: &str) -> u32 {
     match str(key).parse::<u32>() {
         Ok(val) => val,
